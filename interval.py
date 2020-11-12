4751554f4788cdecfd8a3086998389bfc2d98c00
zentirot3007 <phamhoanglanhb@gmail.com>
2020-11-12 22:20:14 +0700

    lan dep trai version 2

--- interval.py
+++ interval.py
@@ -1,9 +1,6 @@
 a=[1,4,5,9,10,2,3]
 interval=0
 
-for i in range (len(a)):
-    for j in range (len(a)):
-        diff=abs(a[i]-a[j])
-        if diff>interval:
-            interval = diff
+a.sort()
+interval=a[len(a)-1]-a[0]
 print(interval)
