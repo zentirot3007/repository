346dce59c9c297c06101dbb954f7f73ccfe63e59
zentirot3007 <phamhoanglanhb@gmail.com>
2020-11-12 22:29:11 +0700

    lan dep trai nhat

--- interval.py
+++ interval.py
@@ -1,6 +1,13 @@
 a=[1,4,5,9,10,2,3]
 interval=0
+minimum=0
+maximum=0
 
-a.sort()
-interval=a[len(a)-1]-a[0]
+for i in range(len(a)):
+    if a[i] < minimum:
+        minimum = a[i]
+    if a[i] > maximum:
+        maximum = a[i]
+        
+interval = maximum - minimum
 print(interval)
